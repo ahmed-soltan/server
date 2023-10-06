6b0f843a05c8214956d379618aea54071f1567c0
ahmedkun <112619689+ahmedkun@users.noreply.github.com>
2023-10-06 11:45:22 +0300

    changes

--- index.js
+++ index.js
@@ -7,26 +7,27 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin:process.env.BASE_URL,
+        origin: process.env.BASE_URL,
     })
 )
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
-app.post("/create-checkout-session", async (req, res)=>{
-    try{
+app.post("/create-checkout-session", async (req, res) => {
+    try {
         console.log(req.body.items)
         const session = await stripe.checkout.sessions.create({
-            payment_method_types:["card"],
-            mode:"payment",
-            line_items:req.body.items&& req.body.items.map(item => {
-                return{
-                    price_data:{
-                        currency:"usd",
-                        product_data:{
-                            name: item.title
+            payment_method_types: ["card"],
+            mode: "payment",
+            line_items: req.body.items && req.body.items.map(item => {
+                return {
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: item.title,
+                            image: item.image
                         },
-                        unit_amount: (item.price)*100,
+                        unit_amount: (item.price) * 100,
 
                     },
                     quantity: item.quantity
@@ -36,11 +37,11 @@ app.post("/create-checkout-session", async (req, res)=>{
             cancel_url: `${process.env.BASEURL}/cancel`
         })
 
-        res.json({url: session.url})
+        res.json({ url: session.url })
 
-    }catch(e){
-            console.log(req.body.items)
-     res.status(500).json({error:e.message})
+    } catch (e) {
+        console.log(req.body.items)
+        res.status(500).json({ error: e.message })
     }
 })
 
